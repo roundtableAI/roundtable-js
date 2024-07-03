5a175c9b23d6e75e3a1ae4a1ea0dc887c5077224
timshell <mayank.agrawal@princeton.edu>
2024-07-03 18:10:03 -0400

    forgot a few mjs

--- core/survey.mjs
+++ core/survey.mjs
@@ -1,5 +1,5 @@
-import Page from "./page.js";
-import Element from "./element.js";
+import Page from "./page.mjs";
+import Element from "./element.mjs";
 
 class Survey {
     constructor(id, title) {
