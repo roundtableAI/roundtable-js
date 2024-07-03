8390f9db61f72c487929a8608ffae78889474963
timshell <mayank.agrawal@princeton.edu>
2024-07-03 18:38:43 -0400

    updating package.json to include files

--- index.mjs
+++ index.mjs
@@ -1,4 +1,7 @@
 import { displayPage, collectData, submitData } from './controller.mjs';
+import http from 'http';
+import fs from 'fs';
+import path from 'path';
 
 // Get the necessary DOM elements
 const questionContainerElement = document.getElementById('question-container');
@@ -8,5 +11,25 @@ displayPage(questionContainerElement);
 
 // Event listener for submit button
 document.getElementById('submit-btn').addEventListener('click', () => {
-    submitData(questionContainerElement);
+  submitData(questionContainerElement);
+});
+
+// Start the HTTP server
+http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error loading index.html');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404);
+    res.end('Not found');
+  }
+}).listen(8080, () => {
+  console.log('Server running at http://localhost:8080/');
 });
